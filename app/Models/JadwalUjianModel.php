7e99b1f3de09cf20189bf9e25ab8ac06c306033e
indahcf <indahcahyafebriani@gmail.com>
2023-06-17 13:47:26 +0700

    87 commits

--- app/Models/JadwalUjianModel.php
+++ app/Models/JadwalUjianModel.php
@@ -11,11 +11,6 @@ class JadwalUjianModel extends Model
     protected $allowedFields    = ['id_kelas', 'id_tahun_akademik', 'tanggal', 'jam_mulai', 'jam_selesai', 'total_hadir', 'jumlah_lju'];
     protected $useTimestamps    = true;
 
-    // public function getJadwalUjian()
-    // {
-    //     return $this->db->table('jadwal_ujian')->join('kelas', 'jadwal_ujian.id_kelas=kelas.id_kelas')->join('jadwal_ruang', 'jadwal_ujian.id_jadwal_ujian=jadwal_ruang.id_jadwal_ujian', 'right')->join('ruang_ujian', 'jadwal_ruang.id_ruang_ujian=ruang_ujian.id_ruang_ujian')->join('matkul', 'kelas.id_matkul=matkul.id_matkul')->join('dosen', 'kelas.id_dosen=dosen.id_dosen')->join('prodi', 'matkul.id_prodi=prodi.id_prodi')->get()->getResultArray();
-    // }
-
     public function getJadwalUjian()
     {
         return $this->db->table('jadwal_ruang')->join('jadwal_ujian', 'jadwal_ujian.id_jadwal_ujian=jadwal_ruang.id_jadwal_ujian', 'right')->join('ruang_ujian', 'jadwal_ruang.id_ruang_ujian=ruang_ujian.id_ruang_ujian', 'left')->join('kelas', 'jadwal_ujian.id_kelas=kelas.id_kelas')->join('matkul', 'kelas.id_matkul=matkul.id_matkul')->join('dosen', 'kelas.id_dosen=dosen.id_dosen')->join('prodi', 'matkul.id_prodi=prodi.id_prodi')->get()->getResultArray();
