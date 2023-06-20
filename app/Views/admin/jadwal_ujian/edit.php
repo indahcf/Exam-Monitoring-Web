cbc106a537d3b89677b63404d650a448a083a402
indahcf <indahcahyafebriani@gmail.com>
2023-06-20 18:31:23 +0700

    93 commits

--- app/Views/admin/jadwal_ujian/edit.php
+++ app/Views/admin/jadwal_ujian/edit.php
@@ -71,7 +71,7 @@
                         <div class="row fg_ruangan_peserta">
                             <div class="form-group col-md-6">
                                 <label for="ruang_ujian">Ruang Ujian 1</label>
-                                <select class="form-control <?= (validation_show_error('ruang_ujian.1')) ? 'is-invalid' : ''; ?>" id="ruang_ujian" name="ruang_ujian[]" data-value="<?= old('ruang_ujian', $ruang_ujian) ?>">
+                                <select class="form-control <?= (validation_show_error('ruang_ujian.1')) ? 'is-invalid' : ''; ?>" id="ruang_ujian" name="ruang_ujian[]">
                                     <option value="">Pilih Ruang Ujian 1</option>
                                 </select>
                                 <div class="invalid-feedback">
@@ -80,7 +80,7 @@
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="jumlah_peserta">Jumlah Peserta Ruang Ujian 1</label>
-                                <input type="number" class="form-control <?= (validation_show_error('jumlah_peserta.1')) ? 'is-invalid' : ''; ?>" id="jumlah_peserta" name="jumlah_peserta[]" value="<?= old('jumlah_peserta.0', $jumlah_peserta[0]['jumlah_peserta']); ?>" placeholder="Jumlah Peserta Ruang Ujian 1" readonly>
+                                <input type="number" class="form-control <?= (validation_show_error('jumlah_peserta.1')) ? 'is-invalid' : ''; ?>" id="jumlah_peserta" name="jumlah_peserta[]" placeholder="Jumlah Peserta Ruang Ujian 1" readonly>
                                 <div class="invalid-feedback">
                                     <?= validation_show_error('jumlah_peserta.1'); ?>
                                 </div>
